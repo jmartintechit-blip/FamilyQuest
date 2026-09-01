c17132c9f840c0bb4cebe7b3c336aa794207de84
Juan Nadales <jmartintech.it@gmail.com>
2026-09-02 00:54:49 +0200

    conectar base de datos SQLite y fcrear tabla usuarios

---
+++ db.js
@@ -0,0 +1,14 @@
+const Database = require('better-sqlite3');
+const db = new Database('app-familia.db');
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT null,
+        email TEXT NOT NULL UNIQUE,
+        puntos_totales INTEGER DEFAULT 0,
+        fecha_creacion TEXT DEFAULT CURRENT_TIMESTAMP
+    )
+`);
+
+module.exports =db;
